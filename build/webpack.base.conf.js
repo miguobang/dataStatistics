de2754298e4a7b3dd951bb415a68e49787a0643a
TUARAN <729922845@qq.com>
2018-11-22 09:51:10 +0800

    add
    init add

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,76 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require("webpack")
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: utils.entries(),
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production' ?
+      config.build.assetsPublicPath :
+      config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
+      'pages': resolve('src/pages'),
+      'components': resolve('src/components'),
+      'jquery': 'jquery' 
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: vueLoaderConfig
+    },
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: [
+        resolve('src'),
+        // resolve('test'),
+        // resolve('node_modules/vue-bulma-tooltip'),
+        // resolve('/node_modules/.2.3.7@element-ui/packages'),
+        // resolve('src/components/index/AsideNav.vue'),
+        // resolve('src/components/err.vue'),
+        resolve('src/pages/history/history.js'),
+
+      ]    
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('img/[name].[hash:7].[ext]')
+      }
+    },
+    {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+      }
+    }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        jquery: "jquery",
+        "window.jQuery": "jquery"
+    })//配置全局设置jquery
+  ]
+}
